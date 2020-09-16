9ba6134528243ca471a07c741a129c7d6c4d0f36
Kristiāns Sauja <kr.sauja@gmail.com>
2020-09-16 11:11:00 +0300

    Adding new class

---
+++ classes.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+Class Person
+{
+    protected string $name;
+    protected string $middlename;
+    protected string $surname;
+
+    public function __construct(string $name, string $middlename, string $surname)
+    {
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->middlename = $middlename;
+    }
+
+    public function export()
+    {
+        $this->name();
+        $this->middlename();
+        $this->surname();
+    }
+}
+
+$person = new Person('Pēteris', 'Māris', 'Andersons');
+var_dump($person);
+?>
